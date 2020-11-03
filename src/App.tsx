ce601532bab258cf7043af4dfd68df0994aecc44
hungwii <hungwii@163.com>
2020-11-03 21:21:56 +0800

    添加样式文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1>hello world</h1>
+        <h2>hello world</h2>
+        <h3>hello world</h3>
+        <h4>hello world</h4>
+        <hr />
+        <code>
+          const a = b
+        </code>
+
         <a
           className="App-link"
           href="https://reactjs.org"
