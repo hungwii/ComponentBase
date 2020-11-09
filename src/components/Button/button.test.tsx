d9b02287228871c54f3dba2edde5950105e2877a
hungwii <hungwii@163.com>
2020-11-09 10:07:24 +0800

    完成Button的测试文件

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,28 +1,53 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent, getByText} from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Button from './Button'
+import Button, {ButtonProps, ButtonType, ButtonSize} from './Button'
+const defaultProps = {
+    onClick: jest.fn()
+}
+const disableProps: ButtonProps = {
+    disabled: true,
+    onClick: jest.fn() //TODO:这句话是什么意思
+}
+
 
 
 describe('test Button component', () => {
 
     it('should render the correct default button', () => {
-        const wrapper = render(<Button>Hello</Button>)
-        const element = wrapper.getByText('Hello')
+        const wrapper = render(<Button {...defaultProps}>Hello</Button>)
+        const element = wrapper.getByText('Hello') as HTMLButtonElement
         expect(element).toBeInTheDocument()
-        expect(element.tagName).toEqual('BUTTON')
+        expect(element.tagName).toEqual('BUTTON') //TOD这个是什么？
         expect(element).toHaveClass('btn btn-default')
+        expect(element.disabled).toBeFalsy()
+        fireEvent.click(element)
+        expect(defaultProps.onClick).toHaveBeenCalled()
     })
 
     it('should render the correct component based on different props', () => {
-
+        const wrapper = render(<Button btnType={ButtonType.Primary} btnSize={ButtonSize.Large}>Hello</Button>) //TODO为什么enum没办法做到双向映射
+        const element  = wrapper.getByText("Hello")
+        expect(element).toBeInTheDocument()
+        expect(element).toHaveClass('btn btn-primary btn-large')
     })
 
     it('should render a link when btnType wquals link and href is provided', () => {
+        const wrapper = render(<Button btnType={ButtonType.Link} href='http://hello'>Link</Button>)
+        const element = wrapper.getByText("Link")
+        expect(element).toBeInTheDocument()
+        expect(element.tagName).toEqual("A") //TODO这个是什么？
+        expect(element).toHaveClass('btn btn-link')
 
     })
 
     it('should render disabled button when disabled ', ()=>{
+        const wrapper = render(<Button disabled={true}>disableButton</Button>)
+        const element = wrapper.getByText("disableButton") as HTMLButtonElement
+        expect(element).toBeInTheDocument()
+        expect(element.disabled).toBeTruthy()
+        fireEvent.click(element)
+        expect(disableProps.onClick).not.toHaveBeenCalled()
 
     })
 })
