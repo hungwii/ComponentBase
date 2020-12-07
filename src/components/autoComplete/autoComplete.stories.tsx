7b30eecd5abba00045211147af695524099166d3
hungwii <hungwii@163.com>
2020-12-07 21:21:22 +0800

    完成自定义模版

--- src/components/autoComplete/autoComplete.stories.tsx
+++ src/components/autoComplete/autoComplete.stories.tsx
@@ -1,20 +1,45 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import AutoComplete, {AutoCompleteProps} from './autoComplete'
+import AutoComplete, {AutoCompleteProps, DataSourceType} from './autoComplete'
+import {action} from '@storybook/addon-actions'
 import { SimpleInput } from '../Input/Input.stories'
 
-const friends = ['pinfa', 'changzhi', 'huangwei', 'hezongdong', 'xujiayi', 'yangjiahua']
-
+interface peopleProps {
+    value: string;
+    number: number;
+}
+// const friends = ['pinfa', 'changzhi', 'huangwei', 'hezongdong', 'xujiayi', 'yangjiahua']
+const nameWithNumber  = [
+    {value:'huangwei', number:1},
+    {value:'yangjiahua', number:2},
+    {value:'xujiayi', number:3},
+    {value:'hezongdong', number:4},
+    {value:'zhaiyaoyong', number:5},
+    {value:'liangchaohua', number:6},
+]
 export default {
     title:"Component/AutoComplete",
     component:AutoComplete,
 } as Meta
 
+const renderOption = (item:DataSourceType<peopleProps>) => {
+    return (
+        <>
+            <h2>name: {item.value}</h2>
+            <p>number: {item.number}</p>
+        </>
+    )
+
+}
+// const handleFetch = (query: string) => {
+//     return friends.filter(name => name.includes(query))
+// }
+ 
 const handleFetch = (query: string) => {
-    return friends.filter(name => name.includes(query))
+    return nameWithNumber.filter(people => people.value.includes(query))
 }
 
-const Template: Story<AutoCompleteProps> = (args) => <AutoComplete fetchSuggestions={handleFetch} {...args}></AutoComplete>
+const Template: Story<AutoCompleteProps> = (args) => <AutoComplete fetchSuggestions={handleFetch}  onSelect={action('selected')} renderOption={renderOption } {...args}></AutoComplete>
 
 export const SimpleAutoComplete = Template.bind({})
-SimpleInput.args = {value:''} //设置默认
+SimpleInput.args = {value:'',} //设置默认
--- src/components/autoComplete/autoComplete.tsx
+++ src/components/autoComplete/autoComplete.tsx
@@ -1,9 +1,17 @@
-import React, { FC, useState, ChangeEvent} from 'react'
+import React, { FC, useState, ChangeEvent, ReactElement} from 'react'
 import Input, { InputProps }from '../Input/Input'
 
+interface DataSourceObject {
+    value:string;
+} 
+
+export type DataSourceType<T = {}> = T & DataSourceObject
+
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-    fetchSuggestions: (str: string) => string[]
-    onSelect ?: (item: string) => void 
+    fetchSuggestions: (str: string) => DataSourceType[];
+    onSelect ?: (item: DataSourceType) => void;
+    renderOption?: (item:DataSourceType) => ReactElement;
+
 }
 
 const AutoComplete: FC<AutoCompleteProps> = (props) => {
@@ -11,14 +19,16 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
         fetchSuggestions,
         value,
         onSelect,
+        renderOption,
         ...restProps
     } = props
 
     //使用useState去监控输入框中数据的改变
     const [inputValue, setInputValue] = useState(value)
-    const [suggestions, setSuggestions] = useState<string[]>([])
+    const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
 
     console.log(suggestions)
+
     const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value.trim()
         setInputValue(value)
@@ -31,14 +41,26 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
         }
     }
 
+    const handleSelect = (item:DataSourceType) => {
+        setInputValue(item.value) //把选择到的放到输入框
+        setSuggestions([]) //之后把推荐框清空
+        if(onSelect) { //这个onSelect触发有什么用
+            onSelect(item)
+        }
+    }
+
+    const renderTemplate = (item:DataSourceType) => {
+        return renderOption ? renderOption(item) : item
+    }
+
     //写一个返回列表的函数
     const generateDropdown = () => {
         return (
             <ul>
                 {suggestions.map((item,index) => {
                     return (
-                        <li key={index}>
-                            {item}
+                        <li key={index} onClick={() => handleSelect(item)}>
+                            {renderTemplate(item)}
                         </li>
                     )
                 })}
